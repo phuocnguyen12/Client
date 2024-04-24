f291548169ec122457f00fc80c10701c67d8f1a0
Nguyễn Thiên Phước <83675378+phuocnguyen12@users.noreply.github.com>
2024-04-24 17:41:11 +0700

    Added Header Edit

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -50,10 +50,10 @@ const HomePage = () => {
             <img src={delivery} alt="device" />
             <h3>free delivery</h3>
             <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut
-              aperiam fugit eos et dolorem eveniet beatae facilis consequatur
-              non. Ea blanditiis minus vitae deleniti dolorum labore quasi
-              aspernatur modi ipsam!
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+              Voluptatum ducimus quisquam voluptas minima blanditiis aliquam
+              quis expedita fugit quibusdam doloribus exercitationem similique
+              enim, eveniet rem dolorem praesentium iure incidunt voluptates?
             </p>
             <Link to={"#"} className="btn">
               read more
