6b476c3ab98fc3a90c5a8fb5ae56ba5dda3dc0b4
Nguyễn Thiên Phước <83675378+phuocnguyen12@users.noreply.github.com>
2024-04-26 05:09:59 +0700

    Banner

--- client/src/pages/Home/HomePage.jsx
+++ client/src/pages/Home/HomePage.jsx
@@ -1,10 +1,14 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import device1 from "../../assets/arduino-Uno.png"
-import device2 from "../../assets/arduino-control.png"
-import device3 from "../../assets/arduino-nano.png"
+import device1 from "../../assets/arduino-Uno.png";
+import device2 from "../../assets/arduino-control.png";
+import device3 from "../../assets/arduino-nano.png";
+import banner1 from "../../assets/category1.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons";
+import {
+  faChevronLeft,
+  faChevronRight,
+} from "@fortawesome/free-solid-svg-icons";
 import "./HomePage.scss";
 
 const HomePage = () => {
@@ -13,33 +17,91 @@ const HomePage = () => {
   const handleSlideChange = (increment) => {
     const newIndex = (index + increment + slides.length) % slides.length;
     setIndex(newIndex);
-  }
+  };
   return (
-    <section className="home">
-      <div className="slides-container">
-        {slides.map((slide, idx) => (
-          <div className={`slides ${index === idx ? "active" : ""}`} key={idx}>
-            <div className="content">
-              <span>device and module</span>
-              <h3>up to 50% off</h3>
-              <Link to="#" className="btn">
-                Shop now
-              </Link>
-            </div>
-            <div className="image">
-              <img src={slide} alt="" />
-            </div>
-          </div>
-        ))}
-      </div>
+    <div>
+      <section className="home">
+        <div className="slides-container">
+          {slides.map((slide, idx) => (
+            <div className={`slide ${index === idx ? "active" : ""}`} key={idx}>
+              <div className="content">
+                <span>device and module</span>
 
-      <div id="next-slide" className="next-icon" onClick={() => handleSlideChange(1)}>
-        <FontAwesomeIcon icon={faChevronRight} />
-      </div>
-      <div id="prev-slide" className="prev-icon" onClick={() => handleSlideChange(-1)}>
-        <FontAwesomeIcon icon={faChevronLeft} />
-      </div>
-    </section>
+                <h3>up to 50% off</h3>
+
+                <Link to="#" className="btn">
+                  shop now
+                </Link>
+              </div>
+
+              <div className="image">
+                <img src={slide} alt="" />
+              </div>
+            </div>
+          ))}
+        </div>
+
+        <div
+          id="next-slide"
+          className="next-icon"
+          onClick={() => handleSlideChange(1)}
+        >
+          <FontAwesomeIcon icon={faChevronRight} />
+        </div>
+
+        <div
+          id="prev-slide"
+          className="prev-icon"
+          onClick={() => handleSlideChange(-1)}
+        >
+          <FontAwesomeIcon icon={faChevronLeft} />
+        </div>
+      </section>
+
+      <section className="banner-container">
+        <div className="banner">
+          <img src={banner1} alt="" />
+
+          <div className="content">
+            <span>limited sales</span>
+
+            <h3>up to 50% off</h3>
+
+            <Link to={"#"} className="btn">
+              shop now
+            </Link>
+          </div>
+        </div>
+
+        <div className="banner">
+          <img src={banner1} alt="" />
+
+          <div className="content">
+            <span>limited sales</span>
+
+            <h3>up to 50% off</h3>
+
+            <Link to={"#"} className="btn">
+              shop now
+            </Link>
+          </div>
+        </div>
+
+        <div className="banner">
+          <img src={banner1} alt="" />
+
+          <div className="content">
+            <span>limited sales</span>
+
+            <h3>up to 50% off</h3>
+
+            <Link to={"#"} className="btn">
+              shop now
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
   );
 };
 
