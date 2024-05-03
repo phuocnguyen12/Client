2e2cefdd936db93103199846968780020d68c3bc
Nguyễn Thiên Phước <83675378+phuocnguyen12@users.noreply.github.com>
2024-05-03 16:46:30 +0700

    Edit and Block User

--- client/src/Components/index.js
+++ client/src/Components/index.js
@@ -4,6 +4,6 @@ export { default as Loading } from "./Loading";
 export { default as AdminSidebar } from "./Sidebar/AdminSidebar";
 export { default as Button } from "./buttons/Button";
 export { default as InputField } from "./inputs/InputField";
-export { default as InputSelect } from "./inputs/InputSelect";
+export { default as InputSelect } from "./inputs/Select";
 export { default as InputForm } from "./inputs/InputForm";
 export { default as Select } from "./inputs/Select";
